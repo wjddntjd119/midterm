d402639362a64e2a3ba5fb2f8d390632e94782d3
정우성 <helloWorld@java.com>
2023-04-20 13:26:49 +0900

    add secondcommit
    두번째 앱구동 성공

---
+++ src/Fruit/Fruit.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+
+function Fruit(){
+  const [fruit, setFruit] = useState("");
+
+  const inputFruit = (e) =>{
+    setFruit("");
+    setFruit(e.target.name);
+
+  }
+  return(
+    <div>
+      <h1>어떤 과일을 좋아하나요?  {fruit}</h1>
+      <button name="사과" onClick={inputFruit} style={{margin:10}}>사과</button>
+      <button name="오렌지" onClick={inputFruit} style={{margin:10}}>오렌지</button>
+      <button name="바나나" onClick={inputFruit} style={{margin:10}}>바나나</button>
+    </div>
+    
+  )
+
+}
+export default Fruit;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Fruit from './Fruit/Fruit';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Fruit/>
   </React.StrictMode>
 );
 
